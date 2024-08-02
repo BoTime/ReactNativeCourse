719c97ac5f8a4f5211593678f39d65ff3ea6f841
botime <25629006+BoTime@users.noreply.github.com>
2024-08-02 11:22:05 -0700

    Configure Typescript paths

---
+++ constants/Colors.ts
@@ -0,0 +1,21 @@
+export const Colors = {
+    light: {
+        text: 'black',
+        welcomeScreenBackground: '#EDEFEE',
+        welcomeScreenText: 'black',
+        headerBackground: '#EE9972',
+        footerBackground: '#EE9972',
+        headerFont: 'black',
+        footerFont: 'black',
+
+    },
+    dark: {
+        text: 'white',
+        welcomeScreenBackground: '#333333',
+        welcomeScreenText: 'white',
+        headerBackground: '#EE9972',
+        footerBackground: '#EE9972',
+        headerFont: 'black',
+        footerFont: 'black',
+    },
+};
