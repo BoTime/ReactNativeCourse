5378c66922fe616b64faa535a6d9c6eda72f5f37
botime <25629006+BoTime@users.noreply.github.com>
2024-08-03 14:29:38 -0700

    React drawer navigation. Working version.

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import { Colors } from "@/constants/Colors";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 import { Ionicons } from "@expo/vector-icons";
 import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 
@@ -11,39 +12,48 @@ import LoginScreen from "./_layout/login";
 import MenuScreen from "./_layout/menu";
 
 // const MyNavigator = createNativeStackNavigator();
-const MyNavigator = createBottomTabNavigator();
+// const MyNavigator = createBottomTabNavigator();
+const MyNavigator = createDrawerNavigator();
 
 function App() {
   return (
-    <NavigationContainer>
-      <MyNavigator.Navigator
-        initialRouteName="Login"
-        screenOptions={({ route }) => ({
-          headerStyle: styles.navigationHeader,
-          headerTitleStyle: styles.navigationHeaderTitle,
-          headerTintColor: Colors.light.navigationHeaderTint,
-          headerTitleAlign: "center",
-          tabBarIcon: ({ focused, color, size }) => {
-            let iconName;
+    // <NavigationContainer>
+    //   <MyNavigator.Navigator
+    //     initialRouteName="Login"
+    //     screenOptions={({ route }) => ({
+    //       headerStyle: styles.navigationHeader,
+    //       headerTitleStyle: styles.navigationHeaderTitle,
+    //       headerTintColor: Colors.light.navigationHeaderTint,
+    //       headerTitleAlign: "center",
+    //       tabBarIcon: ({ focused, color, size }) => {
+    //         let iconName;
 
-            if (route.name === "Welcome") {
-              iconName = focused ? "home" : "home-outline";
-              return <Ionicons name={iconName} size={size} color={color} />;
-            } else if (route.name === "Menu") {
-              iconName = focused ? "menu" : "menu-outline";
-              return <Ionicons name={iconName} size={size} color={color} />;
-            } else if (route.name === "Login") {
-              iconName = "login";
-              return (
-                <MaterialIcons name={iconName} size={size} color={color} />
-              );
-            }
-            return <Ionicons name={iconName} size={size} color={color} />;
-          },
-          tabBarActiveTintColor: Colors.light.headerBackground,
-          tabBarInactiveTintColor: "gray",
-        })}
-      >
+    //         if (route.name === "Welcome") {
+    //           iconName = focused ? "home" : "home-outline";
+    //           return <Ionicons name={iconName} size={size} color={color} />;
+    //         } else if (route.name === "Menu") {
+    //           iconName = focused ? "menu" : "menu-outline";
+    //           return <Ionicons name={iconName} size={size} color={color} />;
+    //         } else if (route.name === "Login") {
+    //           iconName = "login";
+    //           return (
+    //             <MaterialIcons name={iconName} size={size} color={color} />
+    //           );
+    //         }
+    //         return <Ionicons name={iconName} size={size} color={color} />;
+    //       },
+    //       tabBarActiveTintColor: Colors.light.headerBackground,
+    //       tabBarInactiveTintColor: "gray",
+    //     })}
+    //   >
+    //     <MyNavigator.Screen name="Welcome" component={WelcomeScreen} />
+    //     <MyNavigator.Screen name="Login" component={LoginScreen} />
+    //     <MyNavigator.Screen name="Menu" component={MenuScreen} />
+    //   </MyNavigator.Navigator>
+    // </NavigationContainer>
+
+    <NavigationContainer>
+      <MyNavigator.Navigator>
         <MyNavigator.Screen name="Welcome" component={WelcomeScreen} />
         <MyNavigator.Screen name="Login" component={LoginScreen} />
         <MyNavigator.Screen name="Menu" component={MenuScreen} />
